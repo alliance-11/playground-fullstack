aa1b00b0319dfcc3a882be4f3ed144dade969ed8
losrobbos <losrobbos@web.de>
2022-05-16 15:39:02 +0200

    Added signup and login routes

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ The steps:
 - 3: Backend: Setup Environment & Mongoose connection
 - 4: Backend: Setup User & Book Model
 - 5: Backend: Setup /signup & /login routes
-- 6: Backend: Setup Session after Login
-- 7: Frontend: Build signup & login forms
-- 8: Frontend: Call signup / login backend routes from frontend
-- 9: Redirect to User Dashboard page
+- 6: Frontend: Build signup & login forms
+- 7: Frontend: Call signup / login backend routes from frontend
+- 8: Frontend: Redirect to User Dashboard page
+- 9: Backend: Setup Session after Login
+- 10: Backend: Data protection - Return user spefic books only on /books route
+- 11: Backend: Protect routes to authenticated users only (/users)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { NavLink, Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="signup">Signup</NavLink>
+          <NavLink to="login">Login</NavLink>
+          <NavLink to="dashboard">Dashboard</NavLink>
+        </nav>
         <h2>Manage your own book list!</h2>
+        <Routes>
+          <Route path="/" element={<div>HomePage</div>}></Route>
+          <Route path="/signup" element={<div>Signup Form</div>}></Route>
+          <Route path="/login" element={<div>Login Form</div>}></Route>
+          <Route path="/dashboard" element={<div>Dashboard</div>}></Route>
+        </Routes>
+        <form className="form-signup"></form>
       </header>
     </div>
   )
