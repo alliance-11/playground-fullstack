bbbd28053cd90c69bf680b7c82e92425aa9e8a7b
losrobbos <losrobbos@web.de>
2022-05-16 15:22:19 +0200

    Implemented login route BASIC version

--- routes/users.router.js
+++ routes/users.router.js
@@ -14,10 +14,47 @@ usersRouter.get("/", async (req, res) => {
 // create / signup new user
 usersRouter.post("/", async (req, res) => {
 
+  const { email } = req.body
+
+  console.log(email)
+
   // forward data parsed by TÜRSTEHER (express.json middleware)
-  // to database! 
+  // to database!
+
+  const userExisting = await User.findOne({ email })
+
+  // ERROR HANDLING
+  // if user already exists => prevent creation!
+  if(userExisting)
+    return res.status(400).json({error: `User with email ${email} already exists`})
+  
+  // user does not exist => create 
   const userNew = await User.create( req.body ) 
   res.json( userNew )
+})
+
+// POST /users/login
+// login a user
+usersRouter.post("/login", async (req, res) => {
+
+  const { email, password } = req.body
+
+  console.log(req.body)
+
+  // check if user with given email and password EXISTS in database
+  const userFound = await User.findOne({
+    email: email, password
+  })
+
+  // user with given email and password from req.body was not found!
+  // Login fail!
+  if(!userFound)
+    return res.status(400).json({
+      error: "User does not exist! Try with other email / password. Typo?"
+    })
+
+  // user exists! respond with found user
+  res.json( userFound )
 })
 
 
